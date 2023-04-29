3207ed3ab2738726c7a625acfc8251f4e3699b66
Bhuribhat <Bhuribhat@gmail.com>
2023-04-29 10:20:20 +0700

    add avatar profile asset

--- frontend/src/data/Avatar.ts
+++ frontend/src/data/Avatar.ts
@@ -1,7 +1,6 @@
 export const avatars_url = [
-    "https://cdn-icons-png.flaticon.com/512/5556/5556512.png",
-    "https://static.vecteezy.com/system/resources/previews/000/439/863/original/vector-users-icon.jpg",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_hYkbPJqI-VSDeSmelPL9ubOlhxv3ItmBXmsq7U-Nc_y4rZ4BtsX0vjEOeZEAQ2a1aDM&usqp=CAU",
-    "https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745",
-    "https://static.vecteezy.com/system/resources/previews/002/002/403/original/man-with-beard-avatar-character-isolated-icon-free-vector.jpg"
-  ];
+  "/profile1.png",
+  "/profile2.png",
+  "/profile3.png",
+  "/profile4.png",
+];
