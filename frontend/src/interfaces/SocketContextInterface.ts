7bf820b7d522d0c1d0f97a012133efd6ce4bb998
Pras Pitasawad <prasrodo@gmail.com>
2023-04-29 14:27:15 +0700

    add listener and function to useSocket

--- frontend/src/interfaces/SocketContextInterface.ts
+++ frontend/src/interfaces/SocketContextInterface.ts
@@ -1,6 +1,14 @@
 import { ChatRoomInterface } from "./ChatRoomInterface";
+import { MessageInterface } from "./MessageInterface";
+import { UserInterface } from "./UserInterface";
 
 export interface SocketContextInterface {
   socket?: string;
   chatRooms?: ChatRoomInterface[];
+  users?: UserInterface[];
+  messages?: MessageInterface[];
+  createRoom?: (roomName:string)=>void;
+  joinRoom?: (roomName:string)=>void;
+  leaveRoom?: ()=>void;
+  sendMessage?:(message:string)=>void;
 }
